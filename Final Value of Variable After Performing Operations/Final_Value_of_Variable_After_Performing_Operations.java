9ef28f52c041049bafc26da6588ca20c7fad5e5f
avs-shivhare <aashishshivhare97@gmail.com>
2025-03-03 00:24:50 +0530

    correct some code 3 march

--- Final Value of Variable After Performing Operations/Final_Value_of_Variable_After_Performing_Operations.java
+++ Final Value of Variable After Performing Operations/Final_Value_of_Variable_After_Performing_Operations.java
@@ -1,9 +1,10 @@
 class Solution {
-    public int finalValueAfterOperations(String[] arr) {
+    public int finalValueAfterOperations(String[] operations) {
+        int n = operations.length;
         int x = 0;
-        for(int i = 0; i<arr.length; i++) {
-            String s = arr[i];
-            if(s == "X++" || s == "++X" ) x++;
+        for(int i = 0; i<n; i++) {
+            String s = operations[i];
+            if(s.equals("X++") || s.equals("++X")) x++;
             else x--;
         }
         return x;
